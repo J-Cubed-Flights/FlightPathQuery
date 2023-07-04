e70ab34ab325ce4dbb9326da0008fe97ad4109f1
lnsidiou5 <21jason.li@gmail.com>
2023-07-03 23:43:43 -0400

    Created file parsing function (in main). Minor changes to directed graph functions
    In directed graph: added new function to to return a sorted list of the airport names. and made a slight change to addFlight() to improve future functionality.

--- src/directedgraph.h
+++ src/directedgraph.h
@@ -18,7 +18,7 @@ private:
     unordered_map<string, Airport> airports; // string stores the Departure Airport's IATA Code (i.e. JFK)
 public:
     // public function to add a flight into the unordered_map of airports
-    void addFlight(string &airportFullName, string &departCode, string &arriveCode, int flightTime) {
+    void addFlight(string &airportFullName, string &departCode, string &arriveFullName, string &arriveCode, int flightTime) {
         auto it = airports.find(departCode);
 
         if (it != airports.end()) {
@@ -29,6 +29,19 @@ public:
 
             airports.insert(make_pair(departCode, newAirport));
         }
+        //create destination airport if it doesn't exist.
+        //this will prevent later issues in case that airport never had outgoing flights.
+        it = airports.find(arriveCode);
+        if(it == airports.end()) {
+            Airport newAirport(arriveFullName, arriveCode);
+            airports.insert(make_pair(arriveCode, newAirport));
+        }
+    }
+    vector<string> getAirportNames() {
+        // TODO: return a sorted list of airport names in the following format: "[iata] - [airport name]"
+        // I will complete this tomorrow - Jason (7/3/2023)
+        vector<string> result;
+        return result;
     }
 };
 
